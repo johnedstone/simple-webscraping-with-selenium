ea62b088741b0c0bd47b9ad3703d52169aa33e50
johnedstone <johnedstone@gmail.com>
2023-12-20 15:47:08 -0500

    cleaning up and debugging

--- webscraping_script.py
+++ webscraping_script.py
@@ -17,26 +17,33 @@ logging.warning('Reference: https://pythonbasics.org/selenium-wait-for-page-to-l
 logging.warning('Reference: https://www.selenium.dev/selenium/docs/api/py/webdriver_chromium/selenium.webdriver.chromium.webdriver.html?highlight=refresh#selenium.webdriver.chromium.webdriver.ChromiumDriver.refresh')
 logging.warning('Reference: https://www.selenium.dev/documentation/webdriver/actions_api/mouse/')
 
-def wait_for_loading(driver, xpath, plural=False):
-    timeout = 30
-    try:
-        if plural:
-            element_present = EC.presence_of_all_elements_located((By.XPATH, xpath))
-            print(f'Checking for row td {xpath}')
-        else:
-            element_present = EC.presence_of_element_located((By.XPATH, xpath))
-
-        WebDriverWait(driver, timeout).until(element_present)
-    except TimeoutException:
-        print(f'Timed out waiting for {xpath} to load')
-        sys.exit()
-    finally:
-        print(f'Page loaded: {xpath}')
-
 CODE = os.getenv('CODE')
 URL = os.getenv('URL')
 PICKLE_FN = os.getenv('PICKLE_FN', 'current_datadump.pickle')
 READ_PICKLE =  os.getenv('READ_PICKLE', 'no') == 'yes'
+TIMEOUT = float(os.getenv('TIMEOUT', '0.5'))
+TIMEOUT_TO_EXIT = float(os.getenv('TIMEOUT_TO_EXIT', '30'))
+
+logging.warning(f'URL: {URL}')
+logging.warning(f'PICKLE_FN: {PICKLE_FN}')
+logging.warning(f'READ_PICKLE: {READ_PICKLE}')
+logging.warning(f'TIMEOUT: {TIMEOUT}')
+logging.warning(f'TIMEOUT_TO_EXIT: {TIMEOUT_TO_EXIT}')
+
+def wait_for_loading(driver, xpath, plural=False):
+    try:
+        if plural:
+            element_present = EC.presence_of_all_elements_located((By.XPATH, xpath))
+            logging.warning(f'Checking for row td {xpath}')
+        else:
+            element_present = EC.presence_of_element_located((By.XPATH, xpath))
+
+        WebDriverWait(driver, TIMEOUT_TO_EXIT).until(element_present)
+    except TimeoutException:
+        logging.warning(f'Timed out waiting for {xpath} to load')
+        sys.exit()
+    finally:
+        logging.warning(f'Page loaded: {xpath}')
 
 if not READ_PICKLE:
     if not CODE:
@@ -71,13 +78,12 @@ def main():
         xpath = '/html/body/app-root/div/main/div/app-list/div/div[3]/div[1]/table/tbody'
         _ = wait_for_loading(driver, xpath)
         table_body = driver.find_element(By.XPATH, xpath)
-        sleep(0.5)
+        sleep(TIMEOUT)
 
         xpath = ".//tr"
         _ = wait_for_loading(table_body, xpath, plural=True)
-
         table_rows = driver.find_elements(By.XPATH, xpath)
-        sleep(0.5)
+        sleep(TIMEOUT)
 
         for row in table_rows:
             l = [td.text for td in row.find_elements(By.XPATH, ".//td")]
@@ -87,7 +93,7 @@ def main():
                     print(l)
                 else:
                     print(f'DUPLICATE: {l}')
-        print(len(student_list))
+        logging.warning(len(student_list))
         #_ = input('Press enter to continue')
 
         try:
@@ -96,10 +102,10 @@ def main():
 
             next_page_link = driver.find_element(By.XPATH, xpath_next)
             driver.execute_script("arguments[0].click();", next_page_link)
-            sleep(0.5)
+            sleep(TIMEOUT)
 
         except ElementClickInterceptedException as e:
-            print(f'Finished: {type(e)} - {e}')
+            logging.warning(f'Trouble clicking: {type(e)} - {e}')
 
 if __name__ == '__main__':
     try:
@@ -120,6 +126,6 @@ if __name__ == '__main__':
             with open(PICKLE_FN, 'wb') as fh:
                 pickle.dump(student_list, fh)
 
-            print(f'Finished writing {PICKLE_FN}')
+            logging.warning(f'Finished writing {PICKLE_FN}')
 
 # vim: ai et ts=4 sts=4 sw=4 nu
